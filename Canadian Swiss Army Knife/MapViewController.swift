b09e443911502737b5e845c4b3bf934dad69542d
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2022-12-07 17:43:33 -0500

    feat: add map screen

--- Canadian Swiss Army Knife/MapViewController.swift
+++ Canadian Swiss Army Knife/MapViewController.swift
@@ -6,13 +6,18 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: UIViewController {
 
+    @IBOutlet weak var map: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Display the map for Conestoga College - Waterloo
+        let ccCoordinate2D = CLLocationCoordinate2D(latitude: 43.479343, longitude: -80.5185072)
+        map.setRegion(MKCoordinateRegion(center: ccCoordinate2D, span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)), animated: true)
     }
-
 
 }
 
