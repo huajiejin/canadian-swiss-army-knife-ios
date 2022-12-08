676c2a9bea51ba6d9e747b3601068fcf8a759c34
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2022-12-08 00:22:42 -0500

    feat: store unit conversion history with CoreData

--- Canadian Swiss Army Knife/LengthUnitConverterDetailViewController.swift
+++ Canadian Swiss Army Knife/LengthUnitConverterDetailViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import CoreData
 
 class LengthUnitConverterDetailViewController: UIViewController {
     
     @IBOutlet weak var lengthInCentimetersTextField: UITextField!
     @IBOutlet weak var lengthInInchesTextField: UITextField!
+    @IBOutlet weak var unitConversionHistoryTable: UITableView!
     
+    var container: NSPersistentContainer? = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    var unitConversionRecords: [UnitConversionRecord] = []
     var length: Measurement<UnitLength> = Measurement(value: 0, unit: .centimeters)
     var lengthInCentimeters: Double {
         get {
@@ -27,17 +31,54 @@ class LengthUnitConverterDetailViewController: UIViewController {
     @IBAction func cmEditingDidEnd(_ sender: Any) {
         length = Measurement(value: Double(lengthInCentimetersTextField.text ?? "") ?? 0, unit: .centimeters)
         lengthInInchesTextField.text = "\(lengthInInches)"
+        createUnitConversionRecord("\(lengthInCentimeters)", "cm", "\(lengthInInches)", "inch")
     }
     
     @IBAction func inchEditingDidEnd(_ sender: Any) {
         length = Measurement(value: Double(lengthInInchesTextField.text ?? "") ?? 0, unit: .inches)
         lengthInCentimetersTextField.text = "\(lengthInCentimeters)"
+        createUnitConversionRecord("\(lengthInInches)", "inch", "\(lengthInCentimeters)", "cm")
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard container != nil else {
+            fatalError("Persistent container is required.")
+        }
+        
         lengthInCentimetersTextField.delegate = self
         lengthInInchesTextField.delegate = self
+        
+        unitConversionHistoryTable.delegate = self
+        unitConversionHistoryTable.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchUnitConversionRecords()
+    }
+
+    func fetchUnitConversionRecords() {
+        guard let viewContext = container?.viewContext else { return }
+        let unitConversionRecordsRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "UnitConversionRecord")
+        unitConversionRecordsRequest.returnsObjectsAsFaults = false
+        do {
+            unitConversionRecords = try viewContext.fetch(unitConversionRecordsRequest) as! [UnitConversionRecord]
+        }
+        catch {
+            print("SearchRecords request failed.")
+        }
+    }
+    
+    func createUnitConversionRecord(_ originValue: String, _ originUnit: String, _ targetValue: String, _ targetUnit: String) {
+        guard let viewContext = container?.viewContext else { return }
+        let entity = NSEntityDescription.entity(forEntityName: "UnitConversionRecord", in: viewContext)
+        let unitConversionRecord = NSManagedObject(entity: entity!, insertInto: viewContext) as? UnitConversionRecord
+        unitConversionRecord?.setValue(originValue, forKey: "originValue")
+        unitConversionRecord?.setValue(originUnit, forKey: "originUnit")
+        unitConversionRecord?.setValue(targetValue, forKey: "targetValue")
+        unitConversionRecord?.setValue(targetUnit, forKey: "targetUnit")
     }
 }
 
@@ -47,5 +88,23 @@ extension LengthUnitConverterDetailViewController: UITextFieldDelegate {
         let separatorExists = textField.text?.range(of: decimalSeparator) != nil
         let separatorExistsInString = string.range(of: decimalSeparator) != nil
         return !(separatorExists && separatorExistsInString)
+    }
+}
+
+extension LengthUnitConverterDetailViewController: UITableViewDelegate {
+    
+}
+
+extension LengthUnitConverterDetailViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        unitConversionRecords.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UnitConversionHistory", for: indexPath)
+        let unitConversionRecord = unitConversionRecords[indexPath.row]
+        cell.textLabel?.text = (unitConversionRecord.originValue ?? "") + (unitConversionRecord.originUnit ?? "")
+        cell.detailTextLabel?.text = (unitConversionRecord.targetValue ?? "") + (unitConversionRecord.targetUnit ?? "")
+        return cell
     }
 }
