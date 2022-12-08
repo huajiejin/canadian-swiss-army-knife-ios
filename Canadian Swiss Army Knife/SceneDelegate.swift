2bf8268231676fb5fff038562be8e64b7076ab57
Jay (Huajie) Jin <jay.jin.henan@hotmail.com>
2022-12-07 23:36:47 -0500

    feat: store search history with CoreData

--- Canadian Swiss Army Knife/SceneDelegate.swift
+++ Canadian Swiss Army Knife/SceneDelegate.swift
@@ -10,12 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
+    let appDelegate = (UIApplication.shared.delegate as? AppDelegate)
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        if let rootVC = window?.rootViewController as? UITabBarController,
+           let navVC = rootVC.viewControllers?[0] as? UINavigationController,
+           let searchVC = navVC.viewControllers[0] as? SearchViewController
+        {
+            searchVC.container = appDelegate?.persistentContainer
+        }
         guard let _ = (scene as? UIWindowScene) else { return }
     }
 
@@ -47,7 +53,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+        appDelegate?.saveContext()
     }
 
 
--- Canadian Swiss Army Knife/SearchViewController.swift
+++ Canadian Swiss Army Knife/SearchViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import CoreData
 
 class SearchViewController: UIViewController {
     
@@ -15,29 +16,69 @@ class SearchViewController: UIViewController {
             return searchFields[searchFieldPicker.selectedRow(inComponent: 0)]
         }
     }
+    var container: NSPersistentContainer!
+    var searchRecords: [SearchRecord] = []
     
     @IBOutlet weak var searchFieldPicker: UIPickerView!
     @IBOutlet weak var searchTextField: UITextField!
-    
-    
-    @IBAction func search(_ sender: Any) {
-    }
+    @IBOutlet weak var searchHistoryTable: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        guard container != nil else {
+            fatalError("Persistent container is required.")
+        }
+        
         searchFieldPicker.delegate = self
         searchFieldPicker.dataSource = self
+        
+        searchHistoryTable.delegate = self
+        searchHistoryTable.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fetchSearchRecords()
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         switch segue.identifier {
         case "SearchResult":
             let searchResultViewController = segue.destination as! SearchResultViewController
+            let searchText = searchTextField.text ?? ""
+            searchResultViewController.searchText = searchText
             searchResultViewController.searchField = searchField
-            searchResultViewController.searchText = searchTextField.text ?? ""
+            createSearchRecord(searchText, searchField)
+        case "SearchResultFromHistory":
+            if let selectedRow = searchHistoryTable.indexPathForSelectedRow?.row,
+               let searchResultViewController = segue.destination as? SearchResultViewController
+            {
+                let searchRecord = searchRecords[selectedRow]
+                searchResultViewController.searchText = searchRecord.searchText ?? ""
+                searchResultViewController.searchField = searchRecord.searchField ?? ""
+            }
         default:
             preconditionFailure("Unexpected segue id")
         }
+    }
+    
+    func fetchSearchRecords() {
+        let searchRecordsRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "SearchRecord")
+        searchRecordsRequest.returnsObjectsAsFaults = false
+        do {
+            searchRecords = try container.viewContext.fetch(searchRecordsRequest) as! [SearchRecord]
+        }
+        catch {
+            print("SearchRecords request failed.")
+        }
+    }
+    
+    func createSearchRecord(_ searchText: String, _ searchField: String) {
+        let entity = NSEntityDescription.entity(forEntityName: "SearchRecord", in: container.viewContext)
+        let searchRecord = NSManagedObject(entity: entity!, insertInto: container.viewContext) as? SearchRecord
+        searchRecord?.setValue(searchText, forKey: "searchText")
+        searchRecord?.setValue(searchField, forKey: "searchField")
     }
     
 }
@@ -60,3 +101,20 @@ extension SearchViewController: UIPickerViewDataSource {
     }
 }
 
+extension SearchViewController: UITableViewDelegate {
+    
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        searchRecords.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchHistory", for: indexPath)
+        let searchRecord = searchRecords[indexPath.row]
+        cell.textLabel?.text = searchRecord.searchText
+        cell.detailTextLabel?.text = searchRecord.searchField
+        return cell
+    }
+}
